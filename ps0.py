da9ab182bcbc314d45a3428906490197c82860a5
mart00n <awmarton@gmail.com>
2015-05-31 13:45:04 -0400

    Intro to 6.00 CS OCW class problem sets

---
+++ ps0.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env/python
+# Problem set 0
+# mart00n
+# time spent: > 1 hour
+
+# Program takes input for DOB and last name from user, then prints
+dob = input('Please enter your date of birth: ')
+surname = input('Please enter your last name: ')
+print(surname + ' ' + dob)
+
