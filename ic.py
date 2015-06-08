ae556467ed4a4253e1b9cc15e4f455ca5cd5b7b1
mart00n <awmarton@gmail.com>
2015-06-07 20:21:59 -0400

    Added dictionary example, translation program

--- ic.py
+++ ic.py
@@ -86,19 +86,121 @@
 #print('z =', z)
 #print('x =', x)
 
-def f1(x):
-    def g():
-        x = 'abc'
-        assert False
-    x = x + 1
-    print('x =', x)
-    g()
-    return x
+#def f1(x):
+#    def g():
+#        x = 'abc'
+#        assert False
+#    x = x + 1
+#    print('x =', x)
+#    g()
+#    return x
+#
+#x = 3
+#z = f1(x)
+#
+#def isEven(i):
+#    # assumes i is a positive int
+#    # returns True if i is even, otherwise returns false
+#    return i % 2 == 0
 
-x = 3
-z = f1(x)
+#x = 100
+#divisors = ()
+#for i in range(1, x):
+#    if x % i == 0:
+#        divisors = divisors + (i,) # note the , is to define i as part of a tuple
+#print(divisors)
+#
+#Techs = ['MIT', 'Cal Tech']
+#Ivys = ['Harvard', 'Yale', 'Brown']
+#Univs = []
+#Univs.append(Techs)
+#print('Univs =', Univs)
+#
+#Univs.append(Ivys)
+#print('Univs =', Univs)
+#for e in Univs:
+#    print('e =', e)
+#
+#Flat = Techs + Ivys
+#print(Flat)
+#for x in Flat:
+#    print(x)
+#
+#artSchools = ['RISD', 'Harvard']
+#for u2 in artSchools:
+#    if u2 in Flat:
+#        Flat.remove(u2)
+#
+#print('Flat =', Flat)
+#
+#Flat.sort()
+#print('Flat =', Flat)
+#
+#Flat[1] = 'UMass'
+#print('Flat =', Flat)
 
-def isEven(i):
-    # assumes i is a positive int
-    # returns True if i is even, otherwise returns false
-    return i % 2 == 0
+#L1 = [2]
+#L2 = [L1, L1]
+#print('L2 =', L2)
+#
+#L1[0] = 3
+#print('L2 =', L2)
+#
+#L2[0] = 'a'
+#print('L2 =', L2)
+#
+#L1 = [2]
+#print('L2 =', L2)
+#L2 = L1
+#L2[0] = 'a'
+#print('L1 =', L1)
+#print('L2 =', L2)
+
+#def copyList(LSource, LDest):
+#    for e in LSource:
+#        LDest.append(e)
+#        print('LDest =', LDest)
+#L1 = []
+#L2 = [1, 2, 3]
+#copyList(L2, L1)
+#print(L1)
+#print(L2)
+#
+##copyList(L1, L1) # creates an infinite loop! LSource and LDest both point to the same object
+##print(L1)
+
+#D = {1: 'one', 'deux': 'two', 'pi': 3.14159}
+#D1 = D
+#print(D1)
+#D[1] = 'uno'
+#print(D1)
+#for k in D1.keys():
+#    print(k, '=', D1[k])
+
+EtoF = {'bread': 'du pain', 'wine': 'du vin',\
+        'eats': 'mange', 'drinks': 'bois',\
+        'likes': 'aime', 1: 'un',\
+        '6.00': '6.00'}
+
+def translateWord(word, dictionary):
+    if word in dictionary:
+        return dictionary[word]
+    else:
+        return word
+
+def translate(sentence):
+    translation = ''
+    word = ''
+    for c in sentence:
+        if c != ' ':
+            word = word + c
+        else:
+            translation = translation + ' '\
+                          + translateWord(word, EtoF)
+            word = ''
+    return translation[1:] + ' ' + translateWord(word, EtoF)
+
+print(translate('John eats bread'))
+print(translate('Steve drinks wine'))
+print(translate('John likes 6.00'))
+
