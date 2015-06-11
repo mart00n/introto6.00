6b70574ca192e25f4096f081efad74bc90239cfd
mart00n <awmarton@gmail.com>
2015-06-11 00:38:05 -0400

    Altered for python 3

--- ps2/ps2_hangman.py
+++ ps2/ps2_hangman.py
@@ -1,7 +1,7 @@
 # 6.00 Problem Set 3
-# 
+# Name: Andrew Marton 
 # Hangman
-#
+# Time: 
 
 
 # -----------------------------------
@@ -19,14 +19,14 @@ def load_words():
     Depending on the size of the word list, this function may
     take a while to finish.
     """
-    print "Loading word list from file..."
+    print("Loading word list from file...")
     # inFile: file
     inFile = open(WORDLIST_FILENAME, 'r', 0)
     # line: string
     line = inFile.readline()
     # wordlist: list of strings
     wordlist = string.split(line)
-    print "  ", len(wordlist), "words loaded."
+    print("  ", len(wordlist), "words loaded.")
     return wordlist
 
 def choose_word(wordlist):
